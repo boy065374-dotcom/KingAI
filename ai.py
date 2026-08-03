013b92ce6df2af48a6f39516d4a0dcb507320286
boy065374-dotcom <boy065374@gmail.com>
2026-08-03 23:45:58 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,1 +1,15 @@
+import os
+import google.generativeai as genai
 
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def ask_ai(message):
+    try:
+        response = model.generate_content(message)
+        return response.text
+    except Exception as e:
+        return f"حدث خطأ: {e}"
