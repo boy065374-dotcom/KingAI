75624383ba3b163f4f0bbce9cfdafd5be5ca736c
boy065374-dotcom <boy065374@gmail.com>
2026-08-04 03:30:34 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+import os
+
+
+# Bot Settings
+BOT_NAME = "KingAI"
+
+VERSION = "1.0"
+
+
+# Telegram
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+# External Bot
+BACK_TO_BOT = "https://t.me/Diverse11Zbot"
+
+
+# Data
+DATA_FOLDER = "data"
