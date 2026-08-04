f221a503440550fcc002c598714d5ff0c024bca3
boy065374-dotcom <boy065374@gmail.com>
2026-08-04 20:36:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,8 @@ from dotenv import load_dotenv
 
 from telegram.ext import Application
 
-from commands import start
-from commands import help
-from commands import about
-from commands import back_to_bot
-
-from buttons import chats
-from buttons import new_chat
-
+from commands import start, help, about
+from buttons import chats, new_chat
 
 load_dotenv()
 
@@ -18,23 +12,16 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 def main():
-    if not BOT_TOKEN:
-        print("❌ BOT_TOKEN not found in .env")
-        return
-
     app = Application.builder().token(BOT_TOKEN).build()
 
-    # Commands
     start.register(app)
     help.register(app)
     about.register(app)
-    back_to_bot.register(app)
 
-    # Buttons
     chats.register(app)
     new_chat.register(app)
 
-    print("👑 KingAI is running...")
+    print("👑 KingAI Running")
 
     app.run_polling()
 
