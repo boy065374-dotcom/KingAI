da61bb5ac7d45d9f1f4631d7a6a8359ba65ef8a9
boy065374-dotcom <boy065374@gmail.com>
2026-08-04 20:52:51 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,11 +1,15 @@
 import os
+from dotenv import load_dotenv
 import google.generativeai as genai
+
+load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
+
 
 def ask_ai(message):
     try:
