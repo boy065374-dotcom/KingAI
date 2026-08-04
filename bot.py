57ed2dd77b92c196160efb756e668422a1a40390
boy065374-dotcom <boy065374@gmail.com>
2026-08-04 20:58:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-from telegram.ext import Application
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    filters
+)
 
-from commands import start
+from ai import ask_ai
+
 from commands import help
 from commands import about
 
@@ -16,18 +21,43 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
+async def ai_chat(update, context):
+    user_message = update.message.text
+
+    await update.message.reply_text("🤖 جاري التفكير...")
+
+    response = ask_ai(user_message)
+
+    await update.message.reply_text(response)
+
+
 def main():
+
+    if not BOT_TOKEN:
+        print("❌ BOT_TOKEN not found")
+        return
 
     app = Application.builder().token(BOT_TOKEN).build()
 
+
     # Commands
-    start.register(app)
     help.register(app)
     about.register(app)
+
 
     # Buttons
     chats.register(app)
     new_chat.register(app)
+
+
+    # Gemini AI
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            ai_chat
+        )
+    )
+
 
     print("👑 KingAI is running...")
 
