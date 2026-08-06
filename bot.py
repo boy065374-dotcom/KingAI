71fa1f1d1969970b7ba75f0a3adf68f714355efb
boy065374-dotcom <boy065374@gmail.com>
2026-08-06 23:48:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,13 @@ from telegram.ext import (
 
 from ai import ask_ai
 
+# Commands
+from commands import start, chat, help, about, back_to_bot
+
+# Buttons
 from buttons import start as start_button
 from buttons import chats as chats_button
 from buttons import new_chat
-
-from commands import start, chats, help, about, back_to_bot
 
 
 load_dotenv()
@@ -22,15 +24,15 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
-async def ai_message(update, context: ContextTypes.DEFAULT_TYPE):
+async def ai_message(update: ContextTypes.DEFAULT_TYPE, context):
 
-    message = update.message.text
+    user_message = update.message.text
 
     await update.message.reply_text(
         "⏳ جاري التفكير..."
     )
 
-    response = ask_ai(message)
+    response = ask_ai(user_message)
 
     await update.message.reply_text(
         response
@@ -42,21 +44,21 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
 
-    # Commands
+    # Register Commands
     start.register(app)
-    chats.register(app)
+    chat.register(app)
     help.register(app)
     about.register(app)
     back_to_bot.register(app)
 
 
-    # Buttons
+    # Register Buttons
     start_button.register(app)
     chats_button.register(app)
     new_chat.register(app)
 
 
-    # AI messages
+    # AI Handler
     app.add_handler(
         MessageHandler(
             filters.TEXT & ~filters.COMMAND,
