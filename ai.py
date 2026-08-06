c73ee233ed388085d898e641579f0d547a2b55a7
boy065374-dotcom <boy065374@gmail.com>
2026-08-06 22:20:56 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 
-URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={API_KEY}"
 
 
 def ask_ai(message):
