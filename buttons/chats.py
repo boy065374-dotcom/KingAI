e053da40448c0ccf9375a6bbc48f88f497ff1304
boy065374-dotcom <boy065374@gmail.com>
2026-08-06 23:45:05 +0300

    Update chats.py

--- buttons/chats.py
+++ buttons/chats.py
@@ -1,17 +1,18 @@
-from telegram.ext import CallbackQueryHandler
+from telegram import Update
+from telegram.ext import CallbackQueryHandler, ContextTypes
 
 
-async def chats_button(update, context):
-    query = update.callback_query
+async def chats_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    await query.answer()
+    await update.callback_query.answer()
 
-    await query.message.reply_text(
+    await update.callback_query.message.reply_text(
         "💬 لا توجد محادثات محفوظة حاليا"
     )
 
 
 def register(app):
+
     app.add_handler(
         CallbackQueryHandler(
             chats_button,
